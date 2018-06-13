7ccb08bd0b6abc034f58bc21b6c5da633a7f350b
Sam <pengux1@gmail.com>
2018-06-14 00:19:18 +1000

    Begone Exams
    My hair is red; my brain is dead.

---
+++ Notes/Python/FIT1045/ech.py
@@ -0,0 +1,26 @@
+#Selection
+
+if 5 < 7:
+    
+    print("Maths!")
+    
+else:
+    
+    print("What the heck?")
+    
+    
+#Iteration
+
+#Both will print 0-4 in order.
+
+for i in range(0, 5):
+    
+    print(i)
+    
+Num = 0
+
+while Num < 5:
+    
+    print(Num)
+    
+    Num += 1
